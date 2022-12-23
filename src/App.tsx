cbb6d33cfeb3ab563f39fe262a63f89432e80f1a
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-22 18:24:37 -0700

    initiliaze the project
    add vite environment

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [number, setNumber] = useState<string | undefined>(null);
+  return <div>Hello WOrld!</div>;
+};
+
+export default App;
