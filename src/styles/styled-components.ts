4a707c2c5f8fad27f6120ba5b47098fb446bf694
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-26 19:57:32 -0700

    add styled-components
    apply theme provider

---
+++ src/styles/styled-components.ts
@@ -0,0 +1,26 @@
+import * as styledComponents from "styled-components";
+import { ThemedStyledComponentsModule } from "styled-components";
+import { ThemeInterface } from "./theme";
+
+const {
+  default: styled,
+  css,
+  ThemeProvider,
+  keyframes,
+  createGlobalStyle,
+} = styledComponents as ThemedStyledComponentsModule<ThemeInterface>;
+
+export { css, ThemeProvider, keyframes, createGlobalStyle };
+
+export default styled;
+
+// import { createGlobalStyle } from "styled-components";
+
+// const GlobalStyles = createGlobalStyle`
+// body{
+//     margin: 0;
+//     font-family: Open-Sans, Hellvetica, Sans-Serif;
+// }
+// `;
+
+// export default GlobalStyles;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,26 @@
+// const newObj = {
+//   name: "Alex",
+//   age: 24,
+// };
+
+// interface SomeInterface {
+//   alex: string;
+//   age: number;
+// }
+
+// type TypeofKeys = keyof SomeInterface;
+
+// const myName: TypeofKeys = "alex";
+
+interface ThemeInterface {
+  colors: { [key in keyof typeof colors]: string };
+  breakpoints: { [key in keyof typeof breakpoints]: string };
+}
+// The above expression is similar to the example below:
+// for (let key in [1, 2, 3, 4]) {
+//   console.log = key;
+// }
+
 const colors = {
   primary: "red",
   secondary: "blue",
@@ -8,6 +31,6 @@ const breakpoints = {
   tablet: "768px",
   mobile: "480px",
 };
-const theme = { colors, breakpoints };
+const theme: ThemeInterface = { colors, breakpoints };
 
-export default theme;
+export default { theme };
