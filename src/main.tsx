7ead5b3f3e216ad311ff9198fad1f45b505f2345
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-26 20:51:22 -0700

    add global-style

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./style/theme";
+import GlobalStyle from "./style/global-style";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
   </ThemeProvider>
 );
---
+++ src/style/global-style.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        background-color: red;
+    }
+`;
+
+export default GlobalStyle;
