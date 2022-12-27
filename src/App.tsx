ca66eab685722aeb05686cea57b19b5129b163ad
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-26 20:46:09 -0700

    add type annotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
-import Checkbox from "./components/Checkbox";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [firstNumber, setFirstNumber] = useState<number>();
+  const [secondNumber] = useState();
+
+  useEffect(() => {
+    const number = 10;
+    setFirstNumber(10);
+  }, []);
   // const [number, setNumber] = useState<string | undefined>(null);
-  return (
-    <div>
-      <Checkbox text="Hey Man" />
-    </div>
+  function createPair<S, T>(v1: S, v2: T): [S, T] | 0 {
+    if (typeof v1 !== undefined && typeof v2 !== undefined) {
+      return [v1, v2];
+    }
+    return 0;
+  }
+  // console.log(createPair<string, number>("hello", 42));
+
+  const result = createPair<number | undefined, number | undefined>(
+    firstNumber,
+    secondNumber
   );
+  return <div>Checking Versel Deployment</div>;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/theme";
+import { theme } from "./style/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ThemeProvider theme={theme}>
